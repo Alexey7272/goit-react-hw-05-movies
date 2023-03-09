fdd62bc3beb3682adb331a494500c8b51a0a9c79
Alexey7272 <106250222+Alexey7272@users.noreply.github.com>
2023-03-09 13:30:05 +0200

    add backLinkRef

--- src/components/MovieItems/movieItems.js
+++ src/components/MovieItems/movieItems.js
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import { BASE_IMG_URL } from "services/api";
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 
 const MovieItems = ({ movies }) => {
+    const location = useLocation();
 
     return movies.map(({ id, title, poster_path, release_date }) => {
         return (
             <li key={id}>
-                <Link to={`/movies/${id}`}> 
+                <Link to={`/movies/${id}`} state={{ from: location }}> 
                     <img src={ `${BASE_IMG_URL}${poster_path}` } alt={ title } width='100px'></img>
                     <p>{ title }</p>
                     <p> { release_date.slice(0, 4) } </p>
